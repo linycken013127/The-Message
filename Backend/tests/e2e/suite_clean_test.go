e826a6104d3dcbd57c8089297a44d5d4d29d294a
Ken Lin <ken_yc_lin@gss.com.tw>
2026-01-22 09:43:14 +0800

    feat: implement game query feature (Task 4.1)

--- Backend/tests/e2e/suite_clean_test.go
+++ Backend/tests/e2e/suite_clean_test.go
@@ -46,6 +46,7 @@ type CleanArchTestSuite struct {
 	actionPhaseUseCase       usecase.ActionPhaseUseCase
 	intelligencePhaseUseCase usecase.IntelligencePhaseUseCase
 	deckUseCase              usecase.DeckUseCase
+	gameQueryUseCase         usecase.GameQueryUseCase
 }
 
 func (suite *CleanArchTestSuite) SetupSuite() {
@@ -163,6 +164,14 @@ func (suite *CleanArchTestSuite) SetupSuite() {
 		IntelligenceTransferRepo: intelligenceTransferRepo,
 	})
 
+	gameQueryUseCase := usecase.NewGameQueryUseCase(&usecase.GameQueryUseCaseOptions{
+		GameRepo:                 gameRepo,
+		PlayerRepo:               playerRepo,
+		PlayerCardRepo:           playerCardRepo,
+		GamePlayerRepo:           gamePlayerRepo,
+		IntelligenceTransferRepo: intelligenceTransferRepo,
+	})
+
 	// 註冊 HTTP Handlers
 	handler.RegisterGameHandler(&handler.GameHandlerOptions{
 		Engine:        engine,
@@ -203,6 +212,12 @@ func (suite *CleanArchTestSuite) SetupSuite() {
 		IntelligencePhaseUseCase: intelligencePhaseUseCase,
 	})
 
+	handler.RegisterGameQueryHandler(&handler.GameQueryHandlerOptions{
+		Engine:           engine,
+		GameQueryUseCase: gameQueryUseCase,
+		AccountRepo:      accountRepo,
+	})
+
 	server := httptest.NewServer(engine)
 
 	suite.db = db
@@ -221,6 +236,7 @@ func (suite *CleanArchTestSuite) SetupSuite() {
 	suite.intelligenceTransferRepo = intelligenceTransferRepo
 	suite.intelligencePhaseUseCase = intelligencePhaseUseCase
 	suite.deckUseCase = deckUseCase
+	suite.gameQueryUseCase = gameQueryUseCase
 }
 
 func (suite *CleanArchTestSuite) TearDownSuite() {
